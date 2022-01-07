fcee1c106464ab769d8c75009acf500393637ee3
QueenJammie <jammie.lacc@outlook.com>
2022-01-07 14:01:43 -0500

    Added pagee 28 to page 52

---
+++ Script/script-p52.js
@@ -0,0 +1,30 @@
+alert("N'oubliez pas. Dis-moi, qu'en as-tu penser? Merci! : jammie.lacc@outlook.com");
+
+let now = new Date();
+
+let days = ["Dimanche", "Lundi", "Mardi", "Mercredi", "Jeudi", "Vendredi", "Samedi"];
+let day = days[now.getDay()];
+
+let Hours = now.getHours();
+if (Hours < 10) {
+Hours = `0${Hours}`;
+}
+let Minutes = now.getMinutes();
+if (Minutes < 10) {
+Minutes = `0${Minutes}`;
+}
+let today = now.getDate();
+
+let months = ["janvier", "février", "mars", "avril", "mai", "juin", "juillet", "août", "septembre", "octobre", "novembre", "décembre"];
+let Month = months[now.getMonth()];
+
+let dayElement = document.querySelector("#day");
+dayElement.innerHTML = day;
+let hour = document.querySelector("#hour");
+hour.innerHTML = Hours;
+let minutes = document.querySelector("#minutes");
+minutes.innerHTML = Minutes;
+let dates = document.querySelector("#date");
+dates.innerHTML = today;
+let month = document.querySelector("#month");
+month.innerHTML = Month;
